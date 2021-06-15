179b6f11ab651aac150c8469203d33a15b3dddb1
tor3m <torres.m3lly@gmail.com>
2021-06-15 17:45:51 +0200

    New build to production

---
+++ docs/static/js/main.fcfc9209.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptemplate=this.webpackJsonptemplate||[]).push([[0],{10:function(e,t,n){},11:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),r=n(3),s=n.n(r),a=(n(8),n(0));var o=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("main",{children:"HELLO WORLD!"})})};n(10);s.a.render(Object(a.jsx)(i.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.fcfc9209.chunk.js.map
